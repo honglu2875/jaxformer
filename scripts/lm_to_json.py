39ef756fe1b8d1ff3d26567f56105d564f4f4fc3
Honglu Fan <honglu@gpu-st-p4d-24xlarge-76.hpc-1click-prod450.pcluster>
2022-11-24 00:18:14 +0000

    lm to json converter

---
+++ scripts/lm_to_json.py
@@ -0,0 +1,39 @@
+import lm_dataformat as lmd
+from absl import flags
+from absl import app
+import os
+import multiprocessing as mp
+from util import get_files
+from dataset import Dataset
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string('input', None, 'The input dir.')
+flags.DEFINE_string('output', None, 'The output dir.')
+flags.DEFINE_string('filename', 'out.json', 'The output filename.')
+flags.DEFINE_integer('threads', 1, 'Number of threads.')
+
+
+def read_from_file(input_path):
+    reader = lmd.Reader(input_path)
+    lst = list(reader.stream_data())
+    print(f"Reading {input_path} completed.")
+
+    return lst
+
+
+def main(argv):
+    if FLAGS.output:
+        os.makedirs(FLAGS.output, exist_ok=True)
+
+    files = get_files(FLAGS.input)
+    with mp.Pool(processes=FLAGS.threads) as pool:
+        results = pool.map(read_from_file, files)
+
+    docs = []
+    for doc in results:
+        docs.extend(doc)
+
+    dataset = Dataset.from_list([{'text': s} for s in docs])
+    dataset.to_json(FLAGS.output + "/" + FLAGS.filename)
+
+    
